3d3cbafaa2f0091feb5ec8c75c2ea29a6e91315a
Gabriel Cogo <gabrielaurelio33@gmail.com>
2024-07-22 14:58:23 -0300

    Header e footer ajustados

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react'; // Importa a biblioteca React
 import './App.css'; // Importa estilos específicos para o componente App
 import Home from './pages/Home'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 // Define o componente App
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Header/>
+      <main className="Main-content">
       <Home/> 
+      </main>
+      <Footer/>
     </div>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,42 @@
-// src/pages/home/index.tsx
-
 import React, { useState } from 'react'; // Importa a biblioteca React e useState
-import Header from '../../components/Header'; // Importa o componente Header
-import Footer from '../../components/Footer'; // Importa o componente Footer
 import './styles.css'; // Importa estilos específicos para a página inicial
 
 // Define o componente Home
 const Home: React.FC = () => {
   const [text, setText] = useState(''); // Estado para controlar o texto do campo
+  const [numPlayers, setNumPlayers] = useState(1); // Estado para controlar o número de jogadores por equipe
 
   return (
     <div className="home-container"> {/* Container principal da página inicial */}
-      <Header /> {/* Renderiza o componente Header */}
       <div className="content">
-        
         <h1 className="title">Jogadores</h1> {/* Título acima do campo de texto */}
-        <textarea
-          className="text-field"
-          placeholder="Insira os jogadores e seus respectivos níveis conforme ilustrado abaixo:
-          1- Jogador [Nível aqui]
-          2- Jogador [10]
-          3- Jogador [5]
-          4- Jogador [7]
-          5- Jogador [8]
-          .
-          .
-          ."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        
+        <div className="input-container">
+          <textarea
+            className="text-field"
+            placeholder="Insira os jogadores e seus respectivos níveis conforme ilustrado abaixo:
+            1- Jogador [Nível aqui]
+            2- Jogador [10]
+            3- Jogador [5]
+            4- Jogador [7]
+            5- Jogador [8]
+            .
+            .
+            ."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="subtitles-container">
+          <label className="subtitle">Nº de jogadores por equipe:</label>
+          <input
+            type="number"
+            className="number-input"
+            value={numPlayers}
+            onChange={(e) => setNumPlayers(Number(e.target.value))}
+            min="1"
+          />
+        </div>
       </div>
-      <Footer /> {/* Renderiza o componente Footer */}
     </div>
   );
 };
