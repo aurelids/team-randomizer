4c36fa9e3ef602076ee015c3f1ba73cd528e6391
Gabriel Cogo <gabrielaurelio33@gmail.com>
2024-07-22 16:03:55 -0300

    Ajustado o Layout da página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,33 +9,36 @@ const Home: React.FC = () => {
   return (
     <div className="home-container"> {/* Container principal da página inicial */}
       <div className="content">
-        <h1 className="title">Jogadores</h1> {/* Título acima do campo de texto */}
-        <div className="input-container">
-          <textarea
-            className="text-field"
-            placeholder="Insira os jogadores e seus respectivos níveis conforme ilustrado abaixo:
-            1- Jogador [Nível aqui]
-            2- Jogador [10]
-            3- Jogador [5]
-            4- Jogador [7]
-            5- Jogador [8]
-            .
-            .
-            ."
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
+        <div className="left-content">
+          <h1 className="title">Jogadores</h1> {/* Título acima do campo de texto */}
+          <div className="input-container">
+            <textarea
+              className="text-field"
+              placeholder="Insira os jogadores e seus respectivos níveis conforme ilustrado abaixo:
+              1- Jogador [Nível aqui]
+              2- Jogador [10]
+              3- Jogador [5]
+              4- Jogador [7]
+              5- Jogador [8]
+              .
+              .
+              ."
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
+          <div className="subtitles-container">
+            <label className="subtitle">Nº de jogadores por equipe:</label>
+            <input
+              type="number"
+              className="number-input"
+              value={numPlayers}
+              onChange={(e) => setNumPlayers(Number(e.target.value))}
+              min="1"
+            />
+          </div>
         </div>
-        <div className="subtitles-container">
-          <label className="subtitle">Nº de jogadores por equipe:</label>
-          <input
-            type="number"
-            className="number-input"
-            value={numPlayers}
-            onChange={(e) => setNumPlayers(Number(e.target.value))}
-            min="1"
-          />
-        </div>
+        <div className="vertical-divider"></div> {/* Linha divisória */}
       </div>
     </div>
   );
